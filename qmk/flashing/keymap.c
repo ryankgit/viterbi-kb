b6f11cd57d121732241f67a5c78a74e41d703337
Ryan Kirsch <ryankirschcontact@gmail.com>
2023-07-07 21:50:33 -0500

    Update keymap.c

--- qmk/flashing/keymap.c
+++ qmk/flashing/keymap.c
@@ -18,7 +18,7 @@ enum viterbi_layers {
 
 /* _QWERTY
    ,------------------------------------------------,	,------------------------------------------------,
-   | Esc  |   1  |   2  |   3  |   4  |   5  |      |	| PGUP |   6  |   7  |  8   |   9  |   0  | BKSPC|
+   | Esc  |   1  |   2  |   3  |   4  |   5  | BKSPC|	| PGUP |   6  |   7  |  8   |   9  |   0  | BKSPC|
    |------+------+------+------+------+------+------|	|------+------+------+------+------+------+------|
    | Tab  |   Q  |   W  |   E  |   R  |   T  |   	  | | VOL+ |   Y  |   U  |   I  |   O  |   P  |  Del |
    |------+------+------+------+------+------+------|	|------+------+------+------+------+------+------|
@@ -26,7 +26,7 @@ enum viterbi_layers {
    |------+------+------+------+------+------+------|	|------+------+------+------+------+------+------|
    | Shift|   Z  |   X  |   C  |   V  |   B  |    	| | PGDWN|   N  |   M  |  <,  |  >.  |  ?/  | ENTER|
    |------+------+------+------+------+------+------|	|------+------+------+------+------+------+------|
-   | Ctrl | Home | End  |  ALT | MO(1)| Spce |      | | ENTER| Spce |  -_  |  +=  |  {[  |  }]  |  ~`  |
+   | Ctrl | Home | End  |  ALT | MO(1)| Spce | ENTER| | ENTER| Spce |  -_  |  +=  |  {[  |  }]  |  ~`  |
    `------------------------------------------------`	`------------------------------------------------`
 
 	_LOWER
@@ -44,11 +44,11 @@ enum viterbi_layers {
  */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT(
-		KC_ESC,  KC_1,    KC_2,   KC_3,    KC_4,  KC_5,   KC_NO,                  KC_PGUP, KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
+		KC_ESC,  KC_1,    KC_2,   KC_3,    KC_4,  KC_5,   KC_BSPC,                KC_PGUP, KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
 		KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,  KC_T,   KC_NO,                  KC_VOLU, KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
 		KC_BSLS, KC_A,    KC_S,   KC_D,    KC_F,  KC_G,   KC_NO,                  KC_VOLD, KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
 		KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,  KC_B,   KC_NO,                  KC_PGDN, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, 
-		KC_LCTL, KC_HOME, KC_END, KC_LALT, LOWER, KC_SPC, KC_NO,                  KC_ENT,  KC_SPC, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_GRV),
+		KC_LCTL, KC_HOME, KC_END, KC_LALT, LOWER, KC_SPC, KC_ENT,                 KC_ENT,  KC_SPC, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_GRV),
 	[_LOWER] = LAYOUT(
 		KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, MACRO1,               KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, 
 		KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,   KC_NO, WIN_SNIP,             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO, 
@@ -75,7 +75,7 @@ void matrix_scan_user(void) {
 
 void send_git_status(void) {
 	SEND_STRING("git status");
-  _delay_ms(250);
+  _delay_ms(100);
   SEND_STRING(SS_TAP(X_ENTER));
 }
 
@@ -109,5 +109,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
 	}
 
-    return true;
+  return true;
 };
